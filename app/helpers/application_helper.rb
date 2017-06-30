d30c39fa12536a19eb0cee2b6ac7a49b10c94c31
Alex Mekhovov <alex.mehovov@gmail.com>
2017-07-01 00:13:36 +0200

    Add material-kit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     Storytime::BlogPost.published.order(published_at: :desc).limit(3)
   end
 
+  def header_colour
+    # available colors: blue, azure, green, orange, red, purple, gray
+    case @page&.slug
+    when 'home'
+      :green
+    when 'blog'
+      :red
+    else
+      :orange
+    end
+  end
+
 end
