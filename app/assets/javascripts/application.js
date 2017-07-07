1c4d63374102ec9bcd7e74c4a300a9b4a1fe6c73
Alex Mekhovov <alex.mehovov@gmail.com>
2017-07-07 14:03:33 +0200

    Move js libs to ‘lib’ folder, include storytime/application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,32 +11,7 @@
 // about supported directives.
 //
 //= require_self
-//= require jquery
-//= require jquery_ujs
-//= require bootstrap-sprockets
-//= require leather
-//= require cocoon
-//= require jquery-ui/core
-//= require jquery-ui/datepicker
-//= require jquery-ui/position
-//= require jquery-ui/widget
-//= require jquery-ui/resizable
-//= require jquery-ui/sortable
-//= require jquery.ui.timepicker
-//= require jquery.iframe-transport.js
-//= require jquery.fileupload.js
-//= require codemirror
-//= require codemirror/modes/xml
-//= require codemirror/modes/htmlmixed
-//= require codemirror/modes/overlay
-//= require codemirror/addons/fold/xml-fold
-//= require codemirror/addons/edit/closetag
-//= require codemirror/addons/edit/closebrackets
-//= require chosen.jquery
-//= require medium-editor.min
-//= require storytime_admin
+//= require storytime/application.js
 //= require turbolinks
+//= require_tree ./lib
 //= require_tree .
-
-window.Storytime || (window.Storytime = {})
-window.Storytime.Dashboard = {}
